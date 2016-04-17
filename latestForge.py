304dd408ff49924d1a8cd81272d1ec300d8c34cd
Xilef11 <Xilef11@users.noreply.github.com>
2016-04-17 17:39:22 -0400

    added script to check latest forge version on CI server

---
+++ latestForge.py
@@ -0,0 +1,47 @@
+#import urllib
+import urllib2
+import json
+import os
+
+GAMEVER = '1.9'
+
+def getLatestForge(game):
+    #download the versions file
+    #urllib.urlretrieve("http://files.minecraftforge.net/maven/net/minecraftforge/forge/json","forgeVersions.json")
+    #versionFile = open("forgeVersions.json","r")
+
+    #download the versions
+    j = urllib2.urlopen("http://files.minecraftforge.net/maven/net/minecraftforge/forge/json")
+    #load as JSON
+    j_obj = json.load(j)
+
+    b19 = j_obj['mcversion'][game]
+    ver = b19[len(b19)-1]
+    version = str(ver)
+
+    build = j_obj['number'][version]
+    mc = build['mcversion']
+    branch = build['branch']
+    num = build['version']
+
+    gradleVer = mc+"-"+num+("-"+branch if branch!=None else "")
+    return gradleVer
+
+if __name__=='__main__':
+    latest = getLatestForge(GAMEVER)
+    if os.path.isfile("forgeversion.txt") and not os.environ.get('CI') :
+        with open("forgeVersion.txt","r") as f:
+            old = f.read()
+        if(old!=latest and os.path.isfile("hookURL.txt")):
+            #there was an update, trigger the build
+            with open("hookURL.txt","r") as f:
+                hook = f.read()
+            urllib2.urlopen(hook)
+    #else:
+        #drone thing
+        #os.environ.set('')
+        #we'll let gradle handle this for now
+            
+    #save the newest version
+    with open("forgeversion.txt","w") as f:
+        f.write(gradleVer)
